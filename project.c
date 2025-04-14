b695b4cbe43b0d635c127848a1937066b029dc1b
Md Tahmid Wasif <mxw9311@mavs.uta.edu>
2025-04-13 19:07:35 -0500

    main menu

--- project.c
+++ project.c
@@ -2,8 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+void mainMenu(){
+    printf("\nCommunity Skill Mentorship Platform\n");
+    printf("***************************************\n");
+    printf("Main Menu\n");
+    printf("***************************************\n");
+    printf("[1] Sign up\n");
+    printf("[2] Log in\n");
+    printf("[3] Exit\n");
+    printf("***************************************\n");
+}
+
 int main() {
-    printf("Hey");
+    int userInput;
+
+    while (1) {
+        mainMenu();
+        printf("Enter your choice: ");
+        scanf("%d", &userInput);
+
+        switch (userInput) {
+            case 1:
+                //signUp()
+                break;
+            case 2:
+                //login()
+                break;
+            case 3:
+                exit(0);
+            default:
+                printf("\nPlease enter a valid choice.\n");
+                break;
+        }
+
+    }
 
     return 0;
 }
