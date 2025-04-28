d13253e806dbb09c7bd8f6ab91b013a0d884625d
Md Tahmid Wasif <mxw9311@mavs.uta.edu>
2025-04-28 04:21:56 -0500

    run.c

--- run.c
+++ run.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 
 int main(){
-    system("gcc -o main main.c learner.c mentor.c server.c setColor.c getPassword.c validateInput.c safeGitPush.c -lws2_32");
+    system("gcc -o main main.c learner.c mentor.c server.c setColor.c getPassword.c validateInput.c -lws2_32");
     system(".\\main.exe");
 }
